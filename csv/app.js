3520998efcfb5241a3596d3f41df4e7325b47fab
Milgo21 <felixmilgo21@gmail.com>
2023-06-02 13:08:42 +0300

    update

--- csv/app.js
+++ csv/app.js
@@ -4,7 +4,7 @@ const results = [];
 
 // const app = express()
 
-fs.createReadStream('snakes_count_1000.csv')
+fs.createReadStream('data2.csv')
     .pipe(csv({}))
     .on('data', (data) => results.push(data))
     .on('end', ()=> {
